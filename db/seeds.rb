105e27f6c970e59d2439a9d63780116ba020c9bb
ahmed ali <loona.ahmed@gmail.com>
2019-03-04 17:04:57 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User
+# User.destroy_all
 ahmed = User.create!(username: "Ahmed", password: "123456", email: "ahmed@gmail.com")
 
 mike = User.create!(username: "mike", password: "123456", email: "mike@yahoo.com")
 
 # Photo
+# Photo.destroy_all
 photo1 = Photo.create!(user_id: ahmed.id, caption: "foliage")
 photo2 = Photo.create!(user_id: ahmed.id, caption: "card")
 photo3 = Photo.create!(user_id: ahmed.id, caption: "rain")
@@ -22,10 +24,10 @@ photo6 = Photo.create!(user_id: mike.id, caption: "boat")
 
 
 #attach url
-photo1.image.attach(io: File.open("/Users/ahmed/Downloads/photos/foliage.jpg"), filename: "foliage.jpg")
-photo2.image.attach(io: File.open("/Users/ahmed/Downloads/photos/card.jpg"), filename: "card.jpg")
-photo3.image.attach(io: File.open("/Users/ahmed/Downloads/photos/rain.jpg"), filename: "rain.jpg")
+photo1.image.attach(io: File.open("./app/assets/images/seed/foliage.jpg"), filename: "foliage.jpg")
+photo2.image.attach(io: File.open("./app/assets/images/seed/card.jpg"), filename: "card.jpg")
+photo3.image.attach(io: File.open("./app/assets/images/seed/rain.jpg"), filename: "rain.jpg")
 
-photo4.image.attach(io: File.open("/Users/ahmed/Downloads/photos/architecture.jpg"), filename: "architecture.jpg")
-photo5.image.attach(io: File.open("/Users/ahmed/Downloads/photos/beach.jpg"), filename: "beach.jpg")
-photo6.image.attach(io: File.open("/Users/ahmed/Downloads/photos/boat.jpg"), filename: "boat.jpg")
+photo4.image.attach(io: File.open("./app/assets/images/seed/architecture.jpg"), filename: "architecture.jpg")
+photo5.image.attach(io: File.open("./app/assets/images/seed/beach.jpg"), filename: "beach.jpg")
+photo6.image.attach(io: File.open("./app/assets/images/seed/boat.jpg"), filename: "boat.jpg")
